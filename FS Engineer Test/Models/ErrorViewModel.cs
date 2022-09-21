bb2407a13641ae06ec29d18b2ce8196b8f59b970
Samson Adebayo <samson.adebayo@angloamerican.com>
2022-09-21 16:49:45 +0200

    Add project files.

---
+++ FS Engineer Test/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FS_Engineer_Test.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
